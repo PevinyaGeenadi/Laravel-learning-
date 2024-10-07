08af1ecd27e960f56f9bcde6b244441fe0cbefc4
PevinyaGeenadi <haputhanthri200@gmail.com>
2024-10-07 16:09:06 +0530

    making a layout

--- firstproject/resources/views/about.blade.php
+++ firstproject/resources/views/about.blade.php
@@ -1,4 +1,9 @@
 <x-layout>
+    <x-slot:heading>
+        About Page
+    </x-slot:heading>
+
+
 <h1> welcome to About page</h1>
 
 </x-layout>
--- firstproject/resources/views/home.blade.php
+++ firstproject/resources/views/home.blade.php
@@ -3,4 +3,8 @@
         Home Page
     </x-slot:heading>
 
+    <h1>
+        Welcome to the home page
+    </h1>
+
 </x-layout>
